2bcd3a37c464480ac925a3dc2e7c009bbda62d7c
kdevesh <devesh.juit@gmail.com>
2017-10-08 13:10:09 +0530

    Added Pasword Eexception in getAPIkey.py

--- imageapi/management/commands/getAPIkey.py
+++ imageapi/management/commands/getAPIkey.py
@@ -5,6 +5,10 @@ import getpass
 from rest_framework.authtoken.models import Token
 import os
 
+class PasswordError(Exception):
+    def __init__(self,message):
+        super().__init__(message)
+        
 class Command(BaseCommand):
     help = 'Create user and obtain access key'
             
@@ -13,11 +17,17 @@ class Command(BaseCommand):
         password = getpass.getpass("Enter password : ")
         try:
             user = User.objects.get(username=username)
+            password_check=user.check_password(raw_password=password)
+            if(password_check==False):
+                raise PasswordError("You have entered a wrong password.Please try again!!")
             print ("User "+username+" already exists.")
         except User.DoesNotExist:
             user = User.objects.create_user(username=username, password=password)
             user.save()
             print ("User "+username+" created.")
+        except PasswordError as e:
+                print(e)
+                return
         if(os.path.exists(os.path.join(settings.MEDIA_ROOT, username))):
             pass
         else:
