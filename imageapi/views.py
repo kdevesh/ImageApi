6ba7f9e39d657feddd84a5f13a6f0bd004970583
kdevesh <devesh.juit@gmail.com>
2017-10-10 22:47:40 +0530

    Added comments

--- imageapi/views.py
+++ imageapi/views.py
@@ -13,12 +13,12 @@ class ImageInfo(APIView):
     def post(self, request):
         ''' POST method for creating a new image'''
         imageformats = ["jpeg", "JPEG", "jpg", "JPG", "png", "PNG",\
-                       "gif", "GIF", "bmp", "BMP", "tiff", "TIFF"]
+                       "gif", "GIF", "bmp", "BMP", "tiff", "TIFF"]    # All valid image formats
         if 'file' in request.FILES:
             file = request.FILES['file']
             base, ext = os.path.splitext(file.name)
-            ext = ext[1:]
-            if not ext in imageformats:
+            ext = ext[1:]                    # Getting extension of uploaded file
+            if not ext in imageformats:      # Checking whether the extension is valid or not.This checking can be omitted.                          
                 content = {
                     "message": "UnSupported/Invalid image format!!"
                 }
@@ -26,21 +26,16 @@ class ImageInfo(APIView):
             username = request.user.username
             location = os.path.join(settings.MEDIA_ROOT, username)
             file_storage = FileSystemStorage(location=location)
-            filename = file_storage.save(file.name, file)
+            filename = file_storage.save(file.name, file)         # Saving file to media/username folder
             content = {
                 "message": filename+" uploaded successfully!!"
             }
             return JsonResponse(content, status=status.HTTP_201_CREATED)
-        else:
-            content = {
-                "message": "File not found in request"
-            }
-            return JsonResponse(content, status=status.HTTP_400_BAD_REQUEST)
 
     def get(self, request):
         ''' GET method for getting all images linked to the provided access key'''
         username = request.user.username
-        if os.path.exists(os.path.join(settings.MEDIA_ROOT, username)):
+        if os.path.exists(os.path.join(settings.MEDIA_ROOT, username)):        # Checking whether folder media/username exits or not
             images = os.listdir(os.path.join(settings.MEDIA_ROOT, username))
             content = []
             for image in images:
@@ -67,7 +62,7 @@ class ImageDetail(APIView):
                 path = os.path.join(settings.MEDIA_ROOT, username, img)
                 image = open(path, "rb").read()
                 base, ext = os.path.splitext(path)
-                ext = ext[1:]
+                ext = ext[1:]                    # Getting extension of uploaded file
                 return Response(image, content_type="image/"+ext)
             else:
                 content = {
@@ -90,27 +85,22 @@ class ImageDetail(APIView):
             if 'file' in request.FILES:
                 file = request.FILES['file']
                 base, ext = os.path.splitext(file.name)
-                ext = ext[1:]
-                if not ext in imageformats:
+                ext = ext[1:]                     # Getting extension of uploaded file
+                if not ext in imageformats:       # Checking whether the extension is valid or not.This checking can be omitted.
                     content = {
                         "message": "UnSupported/Invalid image format!!"
                     }
                     return JsonResponse(content, status=status.HTTP_400_BAD_REQUEST)
                 path = os.path.join(settings.MEDIA_ROOT, username, img)
-                os.remove(path)
+                os.remove(path)                      # Removing the requested file
                 location = os.path.join(settings.MEDIA_ROOT, username)
                 file_storage = FileSystemStorage(location=location)
-                filename = file_storage.save(img, file)
+                filename = file_storage.save(img, file)        # Saving the uploaded file with the name of requested file
                 content = {
                     "message": filename + " updated successfully!!"
                 }
                 return JsonResponse(content, status=status.HTTP_200_OK)
-            else:
-                content = {
-                    "message": "File not found in request"
-                }
-                return JsonResponse(content, status=status.HTTP_400_BAD_REQUEST)
-        else:
+        else:               # Condition for requested file not found.
             content = {
                 "message": "File not found in request"
             }
@@ -122,12 +112,12 @@ class ImageDetail(APIView):
         images = os.listdir(os.path.join(settings.MEDIA_ROOT, username))
         if img in images:
             path = os.path.join(settings.MEDIA_ROOT, username, img)
-            os.remove(path)
+            os.remove(path)                             # Removing the requested file.
             content = {
                 "message": img+" was deleted successfully."
             }
             return JsonResponse(content, status=status.HTTP_200_OK)
-        else:
+        else:                     # Condition for requested file not found.
             content = {
                 "message": "File not found in request"
                 }
