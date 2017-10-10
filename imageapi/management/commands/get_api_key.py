3e35a556404cdf90f38c4ed231c582af6aafe445
kdevesh <devesh.juit@gmail.com>
2017-10-10 20:40:19 +0530

    Changed name of getAPIkey -> get_api_key.

--- imageapi/management/commands/get_api_key.py
+++ imageapi/management/commands/get_api_key.py
@@ -1,37 +1,38 @@
-from django.core.management.base import BaseCommand, CommandError
+import os
+import getpass
+from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 from django.conf import settings
-import getpass
 from rest_framework.authtoken.models import Token
-import os
+
 
 class PasswordError(Exception):
-    def __init__(self,message):
+    ''' Password Exception Class for throwing password mismtch error. ''' 
+    def __init__(self, message):
         super().__init__(message)
-        
 class Command(BaseCommand):
+    ''' For generating unique token for every user '''
     help = 'Create user and obtain access key'
-            
     def handle(self, *args, **options):
         username = input("Enter username : ")
         password = getpass.getpass("Enter password : ")
         try:
             user = User.objects.get(username=username)
-            password_check=user.check_password(raw_password=password)
-            if(password_check==False):
+            password_check = user.check_password(raw_password=password)
+            if password_check is False:
                 raise PasswordError("You have entered a wrong password.Please try again!!")
-            print ("User "+username+" already exists.")
+            print("User "+username+" already exists.")
         except User.DoesNotExist:
             user = User.objects.create_user(username=username, password=password)
             user.save()
-            print ("User "+username+" created.")
+            print("User "+username+" created.")
         except PasswordError as e:
-                print(e)
-                return
-        if(os.path.exists(os.path.join(settings.MEDIA_ROOT, username))):
+            print(e)
+            return
+        if os.path.exists(os.path.join(settings.MEDIA_ROOT, username)):
             pass
         else:
             os.makedirs(os.path.join(settings.MEDIA_ROOT, username))
         token, created = Token.objects.get_or_create(user=user)
-        print ("Access key for " + username + " : ")
-        print (token)
+        print("Access key for " + username + " : ")
+        print(token)
