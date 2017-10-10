16f9b08aff53b358b6eaa3d2e7ede1b7d081c715
kdevesh <devesh.juit@gmail.com>
2017-10-10 20:54:59 +0530

    Aligned code in accordance with coding standards.

--- imageapi/management/commands/get_api_key.py
+++ imageapi/management/commands/get_api_key.py
@@ -7,12 +7,15 @@ from rest_framework.authtoken.models import Token
 
 
 class PasswordError(Exception):
-    ''' Password Exception Class for throwing password mismtch error. ''' 
+    ''' Password Exception Class for throwing password mismtch error. '''
     def __init__(self, message):
         super().__init__(message)
+
+
 class Command(BaseCommand):
     ''' For generating unique token for every user '''
     help = 'Create user and obtain access key'
+
     def handle(self, *args, **options):
         username = input("Enter username : ")
         password = getpass.getpass("Enter password : ")
@@ -20,14 +23,14 @@ class Command(BaseCommand):
             user = User.objects.get(username=username)
             password_check = user.check_password(raw_password=password)
             if password_check is False:
-                raise PasswordError("You have entered a wrong password.Please try again!!")
+                raise PasswordError("You have entered a wrong password.Please try again")
             print("User "+username+" already exists.")
         except User.DoesNotExist:
             user = User.objects.create_user(username=username, password=password)
             user.save()
             print("User "+username+" created.")
-        except PasswordError as e:
-            print(e)
+        except PasswordError as exception:
+            print(exception)
             return
         if os.path.exists(os.path.join(settings.MEDIA_ROOT, username)):
             pass
