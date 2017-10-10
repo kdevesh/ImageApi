36f528d82727fde9b52422a41bb7e4d7ba877b6b
kdevesh <devesh.juit@gmail.com>
2017-10-10 23:09:19 +0530

    Added comments.

--- imageapi/management/commands/get_api_key.py
+++ imageapi/management/commands/get_api_key.py
@@ -1,3 +1,4 @@
+''' Management command for generating API key or Token'''
 import os
 import getpass
 from django.core.management.base import BaseCommand
@@ -21,18 +22,18 @@ class Command(BaseCommand):
         password = getpass.getpass("Enter password : ")
         try:
             user = User.objects.get(username=username)
-            password_check = user.check_password(raw_password=password)                         # Checking whether entered password matches or not.
+            password_check = user.check_password(raw_password=password)                  # Checking whether entered password matches or not.
             if password_check is False:
                 raise PasswordError("You have entered a wrong password.Please try again")
             print("User "+username+" already exists.")
         except User.DoesNotExist:
-            user = User.objects.create_user(username=username, password=password)               # If user does'nt exist then create one and generate Token
+            user = User.objects.create_user(username=username, password=password)        # If user does'nt exist then create one and generate Token
             user.save()
             print("User "+username+" created.")
-        except PasswordError as exception:                                                      # Catching Password mismatch error
+        except PasswordError as exception:                                               # Catching Password mismatch error
             print(exception)
             return
-        if os.path.exists(os.path.join(settings.MEDIA_ROOT, username)):                         # Check if media/username folder exists if not exists then create
+        if os.path.exists(os.path.join(settings.MEDIA_ROOT, username)):                  # Check if media/username folder exists if not exists then create
             pass
         else:
             os.makedirs(os.path.join(settings.MEDIA_ROOT, username))
